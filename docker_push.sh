4f637d1c573d4e7f09086e2cb2b4c167950d2397
pofider <jan.blaha@hotmail.com>
2018-06-30 22:36:15 +0200

    fix space

--- docker_push.sh
+++ docker_push.sh
@@ -6,7 +6,7 @@ curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add
 sudo apt-get install apt-transport-https -y
 sudo apt-get update && sudo apt-get install -y azure-cli
 
-az login --service-principal --username $AZ_APP_ID --pa ssword $AZ_PASSWORD --tenant $AZ_TENANT
+az login --service-principal --username $AZ_APP_ID --password $AZ_PASSWORD --tenant $AZ_TENANT
 sudo az aks install-cli
 sudo az aks get-credentials --resource-group test --name test
 
