17403bc5ec611800ec0a8773666c2d2e2669b43a
pofider <jan.blaha@hotmail.com>
2018-07-01 12:52:24 +0200

    refactoring

--- push.sh
+++ push.sh
@@ -1,22 +1,26 @@
-# sudo add-apt-repository -y ppa:cpick/hub
-# sudo apt-get update
-# sudo apt-get install -y hub
+echo "push service $1 as image $2:$TRAVIS_TAG"
 
-# hub config --global user.email "honza.pofider@seznam.cz"
-# hub config --global user.name "pofider"
+sudo add-apt-repository -y ppa:cpick/hub
+sudo apt-get update
+sudo apt-get install -y hub
 
-# hub checkout -b update-deployment-${TRAVIS_TAG}
-# hub config remote.origin.url https://pofider:${GITHUB_TOKEN}@github.com/pofider/kubernetes.git
+hub config --global user.email "honza.pofider@seznam.cz"
+hub config --global user.name "pofider"
 
-sed -i 's/'"$2"'\:\(.*\)/'"$2"'\:'"$TRAVIS_TAG"'/' ./config/staging/'"$1"'.yaml
-sed -i 's/'"$2"'\:\(.*\)/'"$2"'\:'"$TRAVIS_TAG"'/' ./config/prod/'"$1"'.yaml
-# hub add config/staging/${1}.yaml
-# hub add config/prod/${1}.yaml
+hub checkout -b update-deployment-${TRAVIS_TAG}
+hub config remote.origin.url https://pofider:${GITHUB_TOKEN}@github.com/pofider/kubernetes.git
 
-# hub commit -m "Update the ${1} application"
+DOCKER_IMAGE="$(echo "$2" | sed 's/[^-A-Za-z0-9_]/\\&/g')"
 
-# echo "push new branch"
-# hub push -u origin update-deployment-${TRAVIS_TAG}
+sed -i 's/'"$DOCKER_IMAGE"'\:\(.*\)/'"$DOCKER_IMAGE"'\:'"$TRAVIS_TAG"'/' ./config/staging/janblaha.yaml
+sed -i 's/'"$DOCKER_IMAGE"'\:\(.*\)/'"$DOCKER_IMAGE"'\:'"$TRAVIS_TAG"'/' ./config/prod/janblaha.yaml
+hub add config/staging/${1}.yaml
+hub add config/prod/${1}.yaml
+
+hub commit -m "Update the $1 application"
+
+echo "push new branch"
+hub push -u origin update-deployment-${TRAVIS_TAG}
 
 # unfortunatelly this crashes on some perm issues
 #hub pull-request -m "Update the janblaha application to ${TRAVIS_TAG}"
