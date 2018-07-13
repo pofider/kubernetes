5fc1f01ee8eb84ad40f33734680ff88b2bbf9734
pofider <jan.blaha@hotmail.com>
2018-07-13 18:25:47 +0200

    fix resource group name

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ sudo apt-get update && sudo apt-get install -y azure-cli
 
 az login --service-principal --username $AZ_APP_ID --password $AZ_PASSWORD --tenant $AZ_TENANT
 sudo az aks install-cli
-sudo az aks get-credentials --resource-group test --name test
+sudo az aks get-credentials --resource-group jsreport --name jsreport
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then 
     echo "deploying to production"
