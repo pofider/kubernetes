4970661d07b0420160233b4f20e7ec30235bae63
pofider <honza.pofider@seznam.cz>
2021-05-31 22:52:16 +0200

    use github token env var

--- push.sh
+++ push.sh
@@ -7,8 +7,8 @@ sudo apt-get install -y hub
 hub config --global user.email "honza.pofider@seznam.cz"
 hub config --global user.name "pofider"
 
-hub checkout -b ${1}-${TRAVIS_TAG}
-hub config remote.origin.url https://pofider:${GITHUB_TOKEN}@github.com/pofider/kubernetes.git
+hub checkout -b ${1}-$TRAVIS_TAG
+hub config remote.origin.url https://pofider:$GITHUB_TOKEN@github.com/pofider/kubernetes.git
 
 DOCKER_IMAGE="$(echo "$2" | sed 's/[^-A-Za-z0-9_]/\\&/g')"
 
@@ -20,7 +20,7 @@ hub add config/prod/${1}.yaml
 hub commit -m "Update the $1 application"
 
 echo "push new branch"
-hub push -u origin ${1}-${TRAVIS_TAG}
+hub push -u origin ${1}-$TRAVIS_TAG
 
 # unfortunatelly this crashes on some perm issues
 #hub pull-request -m "Update the janblaha application to ${TRAVIS_TAG}"
